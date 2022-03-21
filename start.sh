333cebc88c1f17f29010e28d0db93e28e250b462
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-03-21 15:36:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  https://github.com/prince301102/autofilterbot.git /autofilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
